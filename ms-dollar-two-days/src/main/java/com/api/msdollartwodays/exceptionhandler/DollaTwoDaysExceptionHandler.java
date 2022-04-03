efb0c6ade342d9b3e1a842a7f4444f59d4f0418f
Marcus Davi Argolo Ribeiro <marcusdavi@gmail.com>
2022-04-03 15:43:09 -0300

    adjuste docker-compose.yml

--- ms-dollar-two-days/src/main/java/com/api/msdollartwodays/exceptionhandler/DollaTwoDaysExceptionHandler.java
+++ ms-dollar-two-days/src/main/java/com/api/msdollartwodays/exceptionhandler/DollaTwoDaysExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
@@ -36,5 +37,12 @@ public class DollaTwoDaysExceptionHandler extends ResponseEntityExceptionHandler
 
 	return handleExceptionInternal(ex, userMessage, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 
+    }
+    
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(MissingServletRequestParameterException ex,
+	    HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+	return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 }
